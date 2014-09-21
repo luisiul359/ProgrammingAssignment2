25284391be452983b13afbfd00f28d599e72fb08
Luis-Tese <luisiul359@gmail.com>
2014-09-21 12:30:21 +0100

    bug fixed in test.R

--- tests.R
+++ tests.R
@@ -5,8 +5,10 @@ x <- matrix(runif(v*v),nrow=v)
 m <- makeCacheMatrix(x)
 
 ptm <- proc.time()
-dummy<-cacheSolve(m)
+dummy1<-cacheSolve(m)
 proc.time() - ptm
 ptm <- proc.time()
-dummy<-cacheSolve(1m)
+dummy2<-cacheSolve(m)
 proc.time() - ptm
+
+identical(dummy1,dummy2)
